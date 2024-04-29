b0592f5184dbb38052ded5fa9372a5c9bb2e6b1f
uamandeep <asuppal91@gmail.com>
2024-04-30 01:36:04 +0530

    done

--- index.js
+++ index.js
@@ -1,30 +1,88 @@
-const attacks = ['rock', 'paper', 'scissors'];
-let humanScore = 0;
-let computerScore = 0;
 
-function getComputerChoice(){
+
+
+const attacks = ['rock', 'paper', 'scissors']
+let humanScore = 0;
+let cpuScore = 0;
+
+const getComputerChoice = () => {
     let index = Math.floor(Math.random() * attacks.length);
     let cpuAttack = attacks[index];
+
+    return cpuAttack;
 }
 
-function getHumanChoice(){
-    let humanAttack = prompt("Enter your attack ['rock','paper','scissors']").toLocaleLowerCase;
-    if(humanAttack==='rock' || humanAttack==='scissors' || humanAttack==='paper'){
-        return humanAttack;
+const getHumanChoice = () => {
+    let humanAttack = prompt("Choose your attack ['rock', 'paper', 'scissors']: ").toLowerCase();
+
+    switch (humanAttack) {
+        case 'rock'.toLowerCase():
+        break;
+        case 'paper'.toLowerCase():
+        break;
+        case 'scissors'.toLowerCase():
+        break;
+        default:
+            humanAttack = prompt("please try again ['rock', 'paper', 'scissors']: ").toLowerCase();
     }
-    else{
-        return humanAttack = prompt("Please try again: [rock, paper, scissors]");
+
+
+    return humanAttack
+}
+
+const checkWinner = (humanChoice, cpuChoice) => {
+    if (humanChoice === 'rock' && cpuChoice === 'scissors' ||
+    humanChoice === 'paper' && cpuChoice === 'rock'||
+    humanChoice === 'scissors' && cpuChoice === 'paper'
+    ) {
+        humanScore++;
+        console.log('HUMAN score: ', humanScore)
+        console.log('CPU score: ', cpuScore)
+        console.log('human wins')
+    } else if (humanChoice === 'rock' && cpuChoice === 'rock' ||
+    humanChoice === 'paper' && cpuChoice === 'paper'||
+    humanChoice === 'scissors' && cpuChoice === 'scissors'
+    ) {
+        console.log('HUMAN score: ', humanScore)
+        console.log('CPU score: ', cpuScore)
+        console.log('DRAW')
+    } else {
+        cpuScore++;
+        console.log('HUMAN score: ', humanScore)
+        console.log('CPU score: ', cpuScore)
+        console.log('cpu wins')
+    }
+
+    if (humanScore === 5) {
+        console.log('HUMAN WINS THE GAME')
+    }
+
+    if (cpuScore === 5) {
+        console.log('CPU WINS THE GAME')
     }
 }
 
-function playRound(humanChoice, computerChoice){
-    if(humanChoice==='rock' && computerChoice === 'rock' || humanChoice==='scissors' && computerChoice === 'scissors' || humanChoice==='paper' && computerChoice === 'paper' ){
-        console.log('DRAW!');
-    }
-    else if(humanChoice==='rock' && computerChoice === 'paper' || humanChoice==='scissors' && computerChoice === 'rock' || humanChoice==='paper' && computerChoice === 'scissors'){
-        console.log('COMPUTER WINS!');
-    }
-    else{
-        console.log("YOU WIN!");
+const playRound = (humanChoice, cpuChoice) => {
+    console.log(humanChoice, cpuChoice)
+
+    checkWinner(humanChoice, cpuChoice);
+}
+
+const playForFive = (humanChoice, cpuChoice) => {
+    let counter = 5;
+
+    while (counter === 5) {
+        checkWinner(humanChoice, cpuChoice)
+        if (humanScore === 5 || cpuScore === 5) {
+            break;
+        } 
+        
+        playForFive(getHumanChoice(), getComputerChoice())
+    
+        counter++;
+
     }
 }
+playForFive(getHumanChoice(), getComputerChoice())
+
+// playRound(getHumanChoice(), getComputerChoice());
